aa5da8b43cf10f642833b46de94a6f03c3ceb8b9
alamin54017 <alamin4577@gmail.com>
2025-02-03 15:28:08 -0600

    added profile section

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('home/', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm
+from .models import Profile
 
 
 def register(request):
@@ -32,3 +34,16 @@ def logout_view(request):
 @login_required
 def home(request):
     return render(request, 'home.html')
+
+
+@login_required
+def profile(request):
+    profile, created =  Profile.objects.get_or_create(user=request.user)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'profile.html', {'form': form})
